4d72d3b64319362af71befecff8da945a39dd51f
luz <luzma_87@yahoo.com>
2017-10-23 17:46:09 -0500

    muestra PokemonFragment por default

--- app/src/main/java/uio/androidbootcamp/fragmentsscreensize/MainActivity.java
+++ app/src/main/java/uio/androidbootcamp/fragmentsscreensize/MainActivity.java
@@ -1,13 +1,28 @@
 package uio.androidbootcamp.fragmentsscreensize;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        PokemonFragment.OnPokemonFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PokemonFragment pokemonFragment = PokemonFragment.newInstance();
+        FragmentManager fragmentManager = this.getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, pokemonFragment);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onPokemonClicked(String pokemon) {
+        Toast.makeText(this, pokemon, Toast.LENGTH_LONG).show();
     }
 }
